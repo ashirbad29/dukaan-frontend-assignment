39e7f2e0af2a847c29e859d2d777a72093619bca
ashirbad <ashirbadbehera29@gmail.com>
2022-05-14 12:53:28 +0530

    ✨ setup tailwind and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import './App.css';
-
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> Vite + React! + Typescript + Eslint + Prettier setup</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
+    <div className="font-default">
+      <h1 className="text-yellow-400">Hello</h1>
     </div>
   );
 }
